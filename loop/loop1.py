d7041b83e164e2d19c0062a1d3a111f0f1cef2d0
shevass02 <shevass02@gmail.com>
2025-06-21 15:41:31 +0300

    Добавил домашнее задание от 16.06

---
+++ loop/loop1.py
@@ -0,0 +1,22 @@
+#6)Порахуй суму чисел від 1 до 100
+# i=1
+# sum = 0
+# while i<=100:
+#     sum=sum+i
+#     i+=1
+# print(sum)
+
+#9)Порахуй кількість непарних чисел від 1 до 100
+# sum=0
+# for i in range(1, 100):
+#        if i%2!=0:
+#         sum+=1
+# print(sum) 
+       
+#10) Запроси у користувача число n і виведи від 1 до n
+
+# n = int(input("Enter number: "))
+# for i in range(1,n+1):
+#     print(i)
+ 
+    
