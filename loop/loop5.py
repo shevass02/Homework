62b83579bc4110b2e179e24dd5bc5c7df86f7bb8
shevass02 <shevass02@gmail.com>
2025-07-05 21:47:19 +0300

    Дом.задание на понедельник

---
+++ loop/loop5.py
@@ -0,0 +1,18 @@
+# a = 20
+# a-=3
+# a+2
+# print(a)
+# rows = int(input("Enter size: "))
+# i = 0
+# j = 1
+# while i < rows:
+ 
+#     print(" " * rows + '*' * j )
+#     rows -=1
+#     j +=1
+
+# mes = input("Enter mes - ")
+# print(mes.lower().count("a"))
+
+text = input("Enter text - ")
+print(text.count('.'))
